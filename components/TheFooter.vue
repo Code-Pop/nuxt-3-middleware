1c2c5a50417f56290c0c3de6f983bfe3b0ef94bf
Ben Hong <ben@bencodezen.io>
2023-04-02 13:25:31 -0700

    docs: add source code link

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -4,7 +4,9 @@
   <footer class="container-fluid">
     <small>
       Built with <a href="https://picocss.com" class="secondary">Pico</a> •
-      <a href="" class="secondary"> Source code </a>
+      <a href="https://github.com/Code-Pop/nuxt-3-middleware" class="secondary">
+        Source code
+      </a>
     </small>
   </footer>
 </template>
