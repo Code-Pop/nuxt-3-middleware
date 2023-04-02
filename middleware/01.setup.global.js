6e1e693e5ea8d3978412665496094c7f7da647c6
Ben Hong <ben@bencodezen.io>
2023-04-02 16:47:41 -0700

    feat: add example of error handling in middleware

--- middleware/01.setup.global.js
+++ middleware/01.setup.global.js
@@ -1,3 +1,19 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   console.log('--- Setup Global Middleware ---')
+
+  // if (to.path === '/login') {
+  //   throw createError({
+  //     statusCode: 405,
+  //     statusMessage: 'Oh snap it broke!'
+  //   })
+  // }
+
+  // return abortNavigation('Failed at global middleware')
+
+  // try {
+  //   const data = await fetch('/api/this-path')
+  //   return navigateTo('/profile' + data.user.name)
+  // } catch (error) {
+  //   return abortNavigation(error)
+  // }
 })
